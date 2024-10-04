1bdc52b2d2f4bc69cf5e9ae14aca658a759a018c
Nelson-GSJ <nelsongonsiljr@gmail.com>
2024-10-03 23:55:11 -0300

    estudos de python

---
+++ 000_exe.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/share/python
+ls -l
+#fil=input("entre com o nome e extenção do arquivo")
+python3 006.py
---
+++ 004.py
@@ -0,0 +1,2 @@
+with open("004_teste.txt", "w", encoding="utf-8") as arquivo:
+	arquivo.write("primeiro esperimento")
